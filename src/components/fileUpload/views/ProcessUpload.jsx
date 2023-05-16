dc1ec8112ca29269904cbe50ed6bc4ad4cf16ae3
david-aravena <david.aravenap@gmail.com>
2023-05-16 16:41:58 -0400

    refactor ProcessUpload.jsx

--- src/components/fileUpload/views/ProcessUpload.jsx
+++ src/components/fileUpload/views/ProcessUpload.jsx
@@ -2,21 +2,17 @@ import './../css/processUpload.css'
 import {showSizeFile} from './../../../utils/showSizeFile'
 import {HiFolderAdd} from 'react-icons/hi';
 
-
 export const ProcessUpload = ({uploadFilesProgress, filesSelected, linksForDownload}) => {
-    /*
-        ESTO HAY QUE REFACTORIZARLO ENTERO
-    */
 
     const uploadedFileInfo = Object.entries(uploadFilesProgress);
      
     return (
         <div className="containerProcessUpload">
             {filesSelected.length === linksForDownload.length &&
+                /* Boton para subir mas archivos luego de finalizar la subida de archivos */
                 <div className="containerUploadMoreFiles">
                     <button className="buttonUploadMoreFiles" onClick={() => window.location.href = window.location.href}>
                     <HiFolderAdd size={"3rem"} className="iconButtonSelectFiles" />
-
                     </button>
                 </div>        
             }
@@ -24,8 +20,9 @@ export const ProcessUpload = ({uploadFilesProgress, filesSelected, linksForDownl
             {uploadedFileInfo.map((file, index) => {
                 return (
                     <div key={index} className="fileProgress">
+                        {/* Barra con progreso de archivo que se esta subiendo */}
                         <div className="info">
-                            <div className="datas">
+                            <div className="data-file">
                                 <p><span>Nombre</span>: {file[0]}</p>
                                 <p><span>Tamaño</span>: {showSizeFile(file[1][1])}</p>
                                 <p><span>bytes subidos</span>: {showSizeFile(file[1][0])}</p>
@@ -34,10 +31,13 @@ export const ProcessUpload = ({uploadFilesProgress, filesSelected, linksForDownl
                                 <p>{`${Math.round((file[1][0]/file[1][1]*100))}`}%</p>
                             </div>
                         </div>
+
                         <div className="progressbar">
                             <progress value={file[1][0]} max={file[1][1]} style={{minWidth: "10px"}}></progress>
                         </div>
-                        <div className="linkForDownload">
+                        
+                        {/* Barras inferiores con links para descargar archivos subidos */}
+                        <div className="linksForDownload">
                             {linksForDownload.length !== 0 && 
                                 linksForDownload.map((url, index) => {
                                     return(
@@ -52,6 +52,7 @@ export const ProcessUpload = ({uploadFilesProgress, filesSelected, linksForDownl
                                 })
                             }
                         </div>
+
                     </div>
                 )
             })}
